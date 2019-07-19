07426f4ee2264625f383b3a83652ab527c51c5f9
Trevor Wang <trevor.wang@qq.com>
2019-07-19 14:54:56 +0800

    test: test empty state

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,13 +1,9 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
+import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter_todo/app.dart';
 import 'package:flutter_todo/injection.dart';
 
@@ -25,5 +21,14 @@ void main() {
     expect(find.byType(ListView), findsNWidgets(1));
     expect(find.text("content"), findsNWidgets(2));
     expect(find.byType(Checkbox), findsNWidgets(2));
+  });
+
+  testWidgets('Task list page -- empty state', (WidgetTester tester) async {
+    mockEmptyTaskList();
+    await tester.pumpWidget(createApp());
+    await tester.pump();
+    expect(find.byType(ListView), findsNWidgets(1));
+    expect(find.text("content"), findsNothing);
+    expect(find.byType(Checkbox), findsNothing);
   });
 }
