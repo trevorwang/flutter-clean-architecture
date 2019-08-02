04ee42bffa8bb0347d2170e27a00742c7d4811da
Trevor Wang <trevor.wang@qq.com>
2019-08-02 16:41:18 +0800

    feat: enable desktop

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,11 @@ import 'package:dio/dio.dart';
 
 import 'app.dart';
 import 'injection.dart';
+import 'package:flutter/foundation.dart'
+    show debugDefaultTargetPlatformOverride;
 
 void main() {
+  debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
   dio.interceptors.add(LogInterceptor(responseBody: false));
   runApp(createApp(repo));
 }
---
+++ macos/Flutter/flutter_export_environment.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/trevorwang/Dev/flutter/sdk"
+export "FLUTTER_APPLICATION_PATH=/Users/trevorwang/Dev/flutter/workspace/flutter_todo"
+export "FLUTTER_TARGET=/Users/trevorwang/Dev/flutter/workspace/flutter_todo/lib/main.dart"
+export "FLUTTER_BUILD_DIR=/Users/trevorwang/Dev/flutter/workspace/flutter_todo/.dart_tool/flutter_build/fba4bb9c45aa1668844c1d8b70b13409"
+export "FLUTTER_FRAMEWORK_DIR=/Users/trevorwang/Dev/flutter/sdk/bin/cache/artifacts/engine/darwin-x64"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
