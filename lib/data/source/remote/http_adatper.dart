7e14eb1cc77e5521da2eb6fd1240b819057a4330
Trevor Wang <trevor.wang@qq.com>
2020-02-03 10:57:33 +0800

    update dependency to latest

--- lib/data/source/remote/http_adatper.dart
+++ lib/data/source/remote/http_adatper.dart
@@ -1,11 +1,3 @@
 import 'package:dio/dio.dart';
 
 typedef OnDataFetch = Future<ResponseBody> Function();
-
-class HttpAdapter extends DefaultHttpClientAdapter {
-  @override
-  Future<ResponseBody> fetch(RequestOptions options,
-      Stream<List<int>> requestStream, Future cancelFuture) {
-    return super.fetch(options, requestStream, cancelFuture);
-  }
-}
